c775a0a9bedbea6486aaf1d82de66c9682c08fc3
cesargrossl <cesar.grossl@buddemeyer.com.br>
2022-06-14 17:14:49 -0300

    Inserido PDO

---
+++ includes/db_banco.php
@@ -0,0 +1,10 @@
+<?php
+	//06051982 CONEXÕES DO BANCO;
+    if($db_valida == 'srv_portal'){
+        define('DB_NAME_PORTAL',"db_agendix");
+        define('DB_HOST_PORTAL',"localhost");
+        define('DB_USER_PORTAL',"root");
+        define('DB_PASSWD_PORTAL',"");
+    }
+?>
+
--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,7 @@
-<?php
-
-
-?>
-
-<!-- footer content -->
-<footer>
+        <!-- footer content -->
+        <footer>
           <div class="pull-right">
-            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+           Sistema de Agendamento Agendix |  Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
           </div>
           <div class="clearfix"></div>
         </footer>
@@ -15,15 +10,15 @@
     </div>
 
     <!-- jQuery -->
-    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <script src="./vendors/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
-   <script src="../vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+   <script src="./vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <!-- FastClick -->
-    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <script src="./vendors/fastclick/lib/fastclick.js"></script>
     <!-- NProgress -->
-    <script src="../vendors/nprogress/nprogress.js"></script>
+    <script src="./vendors/nprogress/nprogress.js"></script>
     
     <!-- Custom Theme Scripts -->
-    <script src="../build/js/custom.min.js"></script>
+    <script src="./build/js/custom.min.js"></script>
   </body>
 </html>
