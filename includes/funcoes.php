7339a02d26402f31d414e4660907d18df300e603
Cesar Grossl <cgrossl00@gmail.com>
2022-06-25 08:21:06 -0300

    functions

--- includes/funcoes.php
+++ includes/funcoes.php
@@ -1,18 +1,25 @@
 <?php
-    function token($data){
-        return md5($data * 7);
+    //Funções para facilitar o trabalho;
+    class functions{
+        
+        function token($data){
+            return md5($data * 7);
+        }
+
+        function limpar($dados){
+            $dados = str_replace("."," ",str_replace(":"," ",str_replace("]"," ",str_replace("["," ",str_replace("'","",$dados)))));
+            return $dados;
+        }
+
+        function anti_injection($txt){
+            $txt = get_magic_quotes_gpc() == 0 ? addslashes($txt) : $txt;
+            return preg_replace("@(--|\#|\*|;|=)@s", "", $txt);
+        }
+
+        function datainsert($data){
+            $data = str_replace("/", "-", $data);
+            $data = date('Y-m-d',time($data));
+            return $data;
+        } 
     }
-    function limpar($dados){
-        $dados = str_replace("."," ",str_replace(":"," ",str_replace("]"," ",str_replace("["," ",str_replace("'","",$dados)))));
-        return $dados;
-    }
-    function anti_injection($txt){
-        $txt = get_magic_quotes_gpc() == 0 ? addslashes($txt) : $txt;
-        return preg_replace("@(--|\#|\*|;|=)@s", "", $txt);
-    }
-    function datainsert($data){
-        $data = str_replace("/", "-", $data);
-        $data = date('Y-m-d',time($data));
-        return $data;
-    } 
 ?>
