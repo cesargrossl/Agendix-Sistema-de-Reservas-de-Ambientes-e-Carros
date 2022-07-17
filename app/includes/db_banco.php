70987fb662403ce621ea66976b7d84f4e94af65e
Cesar Grossl <cgrossl00@gmail.com>
2022-07-17 17:42:45 -0300

    docker

--- app/includes/db_banco.php
+++ app/includes/db_banco.php
@@ -2,9 +2,9 @@
 	//06051982 CONEXÕES DO BANCO;
     if($db_valida == 'srv_portal'){
         define('DB_NAME_PORTAL',"db_agendix");
-        define('DB_HOST_PORTAL',"localhost");
-        define('DB_USER_PORTAL',"root");
-        define('DB_PASSWD_PORTAL',"");
+        define('DB_HOST_PORTAL',"mysql");
+        define('DB_USER_PORTAL',$_ENV['MYSQL_USER']);
+        define('DB_PASSWD_PORTAL',$_ENV['MYSQL_PASS']);
     }
 ?>
 
